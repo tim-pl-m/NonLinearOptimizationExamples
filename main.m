7d1d50cc69ec608c31b2a29c45ee97bb0a052a4c
jamir414 <jamir.rajbhandari@hotmail.com>
2015-05-12 00:31:26 +0200

    newtonverf.m file updated and main.m file added

---
+++ main.m
@@ -0,0 +1,54 @@
+
+
+%% Rosenbrock function:
+fun_rosenbrock = @(x) [100*(x(2) - x(1)^2)^2 + (1-x(1))^2];
+gradfun_rosenbrock = @(x) [400*x(1)^2 - 400*x(1)*x(2) + 2*x(1)-2; 200*(x(2) - x(1)^2)];
+hessfun_rosenbrock = @(x) [1200*x(1)^2 - 400 * x(2) + 2, -400*x(1); -400*x(1), 200];
+x_rosenbrock = zeros(10);
+y_rosenbrock = zeros(10);
+init_values = linspace(0.1,1,10);
+for i = 1:10
+    for j = 1:10
+       x = newtonverf([init_values(i);init_values(j)],fun_rosenbrock,gradfun_rosenbrock,hessfun_rosenbrock);
+       x_rosenbrock(i,j) = x(1,1);
+       y_rosenbrock(i,j) = x(2,1);
+    end
+end
+% For initial values (0.5,0.5), (1,0.1),(1,0.2), ....,(1,1), the method
+% converges to stationary point (1,1)
+
+%% Beale function:
+fun_beale = @(x) [1.5-x(1)*(1-x(2))^2+(2.25-x(1)*(1-x(2)^2))^2+(2.625-x(1)*(1-x(2)^3))^2];
+gradfun_beale = @(x) [2*x(1)*x(2)^6 + 2*x(1)*x(2)^4 - 4*x(1)*x(2)^3 - 4*x(1)*x(2)^2 + 6*x(1) + 5.25*x(2)^3 + 4.5*x(2)^2 - 2*x(2) - 10.75;...
+                      6*x(1)^2*x(2)^5 + 4*x(1)^2*x(2)^3-6*x(1)^2*x(2)^2 + 15.75*x(1)*x(2)^2 + 9*x(1)*x(2)-2*x(1)+2*x(2)+1];
+hessfun_beale = @(x) [2*x(2)^6+2*x(2)^4-4*x(2)^3-4*x(2)^2+6,...
+                      12*x(1)*x(2)^5+8*x(1)*x(2)^5 + 8*x(1)*x(2)^3 - 12*x(1)*x(2)^2 - 8*x(1)*x(2)+15.75*x(2)^2+9*x(2)-2;...
+                      12*x(1)*x(2)^5+8*x(1)*x(2)^5 + 8*x(1)*x(2)^3 - 12*x(1)*x(2)^2 - 8*x(1)*x(2)+15.75*x(2)^2+9*x(2)-2,...
+                      30*x(1)^2*x(2) + 12*x(1)^2*x(2)^2-12*x(1)^2*x(2)+31.5*x(2)+9];
+x_beale = zeros(10);
+y_beale = zeros(10);
+for i = 1:10
+    for j = 1:10
+        x = newtonverf([init_values(i);init_values(j)],fun_beale,gradfun_beale,hessfun_beale);
+        x_beale(i,j) = x(1,1);
+        y_beale(i,j) = x(2,1);
+    end
+end
+
+%For initial values (0.1:0.1:1,0.1:0.1:0.3),(0.2:0.1:1,0.4),
+%(0.4:0.1:1,0.5), (0.7:0.1:1,0.6), (1,0.8), the method converges to
+%stationary point (1.8458,0.1344)
+%Other stationary point is (0.0001,-26.2055)
+%% Spelluci function: 
+fun_spelluci = @(x) [2*x(1)^3+x(2)^2+x(1)^2*x(2)^2+4*x(1)*x(2)+3]; 
+gradfun_spelluci = @(x)[6*x(1)^2 + 2*x(1)* x(2)^2 + 4 * x(2); 2 * x(2) + 2 * x(1)^2 * x(2) + 4 * x(1)];
+hessfun_spelluci = @(x) [12*x(1) + 2*x(2),4*x(1)*x(2)+4;4*x(1)*x(2)+4, 2*x(1)^2 + 2];
+x_spelluci = zeros(10);
+y_spelluci = zeros(10);
+for i = 1:10
+    for j = 1:10
+        x = newtonverf([init_values(i);init_values(j)],fun_spelluci,gradfun_spelluci,hessfun_spelluci);
+        x_spelluci(i,j) = x(1,1);
+        y_spelluci(i,j) = x(2,1);
+    end
+end
