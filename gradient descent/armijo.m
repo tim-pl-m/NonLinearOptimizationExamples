5b56f01b745a6a50d3eec351b286bd58a13fb8d9
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2015-05-27 17:40:15 +0200

    add widening of stepsize

--- gradient descent/armijo.m
+++ gradient descent/armijo.m
@@ -2,11 +2,20 @@ function [ sigma ] = armijo(f, gradf, x, delta)
 betta1 = 1/2;
 betta2 = 1/2;
 sigma = 1;
-gamma = 1;
+%gamma = 1;
 
+%reduce stepsize if too big; with negative directions
 while (f(x - sigma * gradf(x))> (f(x) - delta*sigma*dot(gradf(x)' ,gradf(x))))
+    % take the middle
     sigma = 0.5 * (betta1 + betta2) * sigma;
 end;
+
+% widen stepsize if too small; with negative directions
+% while (f(x - sigma * gradf(x))< (f(x) - delta*sigma*dot(gradf(x)' ,gradf(x))))
+%     % take the middle
+%     sigma = 2 * sigma
+% end;
+
 
 end
 
